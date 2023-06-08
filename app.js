7908a388d29856634ed7867d4a62e8028724e052
NicoleCobos <nicoledanielacv16@gmail.com>
2023-06-07 23:48:56 -0500

    Add Cajero

---
+++ app.js
@@ -0,0 +1,68 @@
+let accounts = [
+    { id: 1, password: "1234", balance: 500 },
+    { id: 2, password: "0000", balance: 980 },
+    { id: 3, password: "2426", balance: 300 }
+];
+
+let selectedAccount = 0;
+let currentAction = 0;
+
+function showPasswordPrompt(accountIndex) {
+    selectedAccount = accounts[accountIndex];
+    let password = prompt("Ingresa la contraseña para la cuenta " + selectedAccount.id + ":");
+    if (password === selectedAccount.password) {
+        document.getElementById("accountSelection").style.display = "none";
+        document.getElementById("actions").style.display = "block";
+        document.getElementById("inputAmount").style.display = "block";
+        document.getElementById("message").innerHTML = "Cuenta seleccionada: " + selectedAccount.id;
+    } else {
+        selectedAccount = 0;
+        alert("Contraseña incorrecta. Intenta nuevamente.");
+    }
+}
+
+function showAmountPrompt() {
+    currentAction = "deposit";
+    document.getElementById("message").innerHTML = "Ingrese el monto a depositar:";
+}
+
+function showWithdrawPrompt() {
+    currentAction = "withdraw";
+    document.getElementById("message").innerHTML = "Ingrese el monto a retirar:";
+}
+
+function performAction() {
+    let amount = parseInt(document.getElementById("amount").value);
+    if (currentAction === "deposit") {
+        deposit(amount);
+        
+    } else if (currentAction === "withdraw") {
+        withdraw(amount);
+        
+    }
+    document.getElementById("amount").value = "";
+}
+
+function deposit(amount) {
+    let suma=selectedAccount.balance + amount
+    if (suma <901){  
+        selectedAccount.balance +=amount
+        document.getElementById("message").innerHTML = "Depósito exitoso. Nuevo saldo: " + selectedAccount.balance;
+    }else{
+        document.getElementById("message").innerHTML = "Ingrese un monto válido supera lo permitido en la cuenta.";
+    }
+}
+
+
+function withdraw(amount) {
+    let resta = selectedAccount.balance -amount 
+    if (resta >9){
+        selectedAccount.balance -=amount
+        document.getElementById("message").innerHTML = "Retiro exitoso. Nuevo saldo: " + selectedAccount.balance;
+    } else {
+        document.getElementById("message").innerHTML = "Saldo insuficiente.";
+    }   
+}
+function checkBalance() {
+    document.getElementById("message").innerHTML = "Saldo actual: " + selectedAccount.balance;
+}
